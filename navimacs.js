816d0ea33e13309eb47c527fac1d2c1da4980e9e
Denis Rechkunov <mail@pragmader.me>
2017-09-30 15:36:46 +0200

    Fix #2: override browser shortcuts and global refactoring.
    Changed how all the keystrokes are handled which makes it more
    scalable and extendible.

--- navimacs.js
+++ navimacs.js
@@ -2,73 +2,83 @@
 
 const stepsPerPage = 5;
 
+const keyStrokes = {
+		'C-78': scroll((x, y) => { return {x, y: y + getScrollSteps().y}; }), // C-n, scroll step down
+		'C-80': scroll((x, y) => { return {x, y: y - getScrollSteps().y}; }), // C-p, scroll step up
+		'C-70': scroll((x, y) => { return {x: x + getScrollSteps().x, y}; }), // C-f, scroll step right
+		'C-66': scroll((x, y) => { return {x: x - getScrollSteps().x, y}; }), // C-b, scroll step left
+		'C-86': scroll((x, y) => { return {x, y: y + window.innerHeight}; }), // C-v, scroll page down
+		'M-86': scroll((x, y) => { return {x, y: y - window.innerHeight}; }), // M-v, scroll page up
+		'M-S-188': scroll((x, y) => { return {x, y: 0}; }), // M-<, scroll top
+		'M-S-190': scroll((x, y) => { return {x, y: window.scrollMaxY}; }), // M->, scroll bottom
+};
+
 document.addEventListener('keydown', onKeyDown);
 
 function onKeyDown(event) {
-		const scrollYStep = window.innerHeight / stepsPerPage;
-		const scrollXStep = window.innerWidth / stepsPerPage;
-		var newX = window.scrollX;
-		var newY = window.scrollY;
-
-		if (event.ctrlKey) {
-				switch(event.keyCode) {
-				case 78:
-						// C-n, scroll step down
-						newY += scrollYStep;
-						break;
-				case 80:
-						// C-p, scroll step up
-						newY -= scrollYStep;
-						break;
-				case 70:
-						// C-f, scroll step right
-						newX += scrollXStep;
-						break;
-				case 66:
-						// C-b, scroll step left
-						newX -= scrollXStep;
-						break;
-				case 86:
-						// C-v, scroll page down
-						newY += window.innerHeight;
-						break;
-				}
-		}
+		var keyStroke = '';
 
 		if (event.altKey || event.metaKey) {
-				if (event.shiftKey) {
-						switch(event.keyCode) {
-						case 188:
-								// M-<, scroll top
-								newY = 0;
-								break;
-						case 190:
-								// M->, scroll bottom
-								newY = window.scrollMaxY;
-								break;
-						}
-				} else {
-						switch(event.keyCode) {
-						case 86:
-								// C-v, scroll page up
-								newY -= window.innerHeight;
-								break;
-						}
+				keyStroke += 'M-';
+		}
+
+
+		if (event.ctrlKey) {
+				keyStroke += 'C-';
+		}
+
+		if (event.shiftKey) {
+				keyStroke += 'S-';
+		}
+
+		keyStroke += event.keyCode;
+
+		console.log('Trying keystroke ' + keyStroke);
+		// if the keystroke is defined
+		if (!keyStrokes.hasOwnProperty(keyStroke)) {
+				console.log('Not found');
+				return;
+		}
+
+
+		// if the keystroke is handled we prevent the default behavior
+		if (keyStrokes[keyStroke](event)) {
+				console.log('handled');
+				event.preventDefault();
+		}
+}
+
+function getScrollSteps() {
+		return {
+				x: window.innerWidth / stepsPerPage,
+				y: window.innerHeight / stepsPerPage
+		};
+}
+
+function scroll(mutator) {
+		return event => {
+				const element = event.target;
+				// the scroll hotkeys are not active while editing text, because in some systems
+				// the emacs hotkeys are used for navigation in text
+				if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA' || element.contentEditable === 'true') {
+						return false;
 				}
-		}
 
-		if (newX < 0) {
-				newX = 0;
-		}
-		if (newY < 0) {
-				newY = 0;
-		}
-		if (newY > window.scrollMaxY) {
-				newY = window.scrollMaxY;
-		}
-		if (newX > window.scrollMaxX) {
-				newX = window.scrollMaxX;
-		}
+				const xy = mutator(window.scrollX, window.scrollY);
+				if (xy.x < 0) {
+						xy.x = 0;
+				}
+				if (xy.y < 0) {
+						xy.y = 0;
+				}
+				if (xy.y > window.scrollMaxY) {
+						xy.y = window.scrollMaxY;
+				}
+				if (xy.x > window.scrollMaxX) {
+						xy.x = window.scrollMaxX;
+				}
 
-		window.scrollTo(newX, newY);
+				window.scrollTo(xy.x, xy.y);
+				return true;
+		}
 }
