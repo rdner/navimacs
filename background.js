f778a7818f8df91a8b5c4d3e7c681408f1d00e1c
Denis Rechkunov <mail@pragmader.me>
2017-09-30 19:37:17 +0200

    Add support for long keystrokes, e.g. close/open tab.
    C-x, f -- opens a new tab.
    C-x, k -- closes the current tab.

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener((message, sender) => {
+		switch(message.action) {
+		case 'closeTab':
+				chrome.tabs.remove(sender.tab.id);
+				break;
+		case 'openNewTab':
+				chrome.tabs.create({active: true});
+				break;
+		}
+});
--- navimacs.js
+++ navimacs.js
@@ -1,51 +1,51 @@
-'use strict';
-
 const stepsPerPage = 5;
 
 const keyStrokes = {
-		'C-78': scroll((x, y) => { return {x, y: y + getScrollSteps().y}; }), // C-n, scroll step down
-		'C-80': scroll((x, y) => { return {x, y: y - getScrollSteps().y}; }), // C-p, scroll step up
-		'C-70': scroll((x, y) => { return {x: x + getScrollSteps().x, y}; }), // C-f, scroll step right
-		'C-66': scroll((x, y) => { return {x: x - getScrollSteps().x, y}; }), // C-b, scroll step left
-		'C-86': scroll((x, y) => { return {x, y: y + window.innerHeight}; }), // C-v, scroll page down
-		'M-86': scroll((x, y) => { return {x, y: y - window.innerHeight}; }), // M-v, scroll page up
-		'M-S-188': scroll((x, y) => { return {x, y: 0}; }), // M-<, scroll top
-		'M-S-190': scroll((x, y) => { return {x, y: window.scrollMaxY}; }), // M->, scroll bottom
+		'C-78': scroll((x, y) => { return {x, y: y + getScrollSteps().y}; }), // C-n -- scroll step down
+		'C-80': scroll((x, y) => { return {x, y: y - getScrollSteps().y}; }), // C-p -- scroll step up
+		'C-70': scroll((x, y) => { return {x: x + getScrollSteps().x, y}; }), // C-f -- scroll step right
+		'C-66': scroll((x, y) => { return {x: x - getScrollSteps().x, y}; }), // C-b -- scroll step left
+		'C-86': scroll((x, y) => { return {x, y: y + window.innerHeight}; }), // C-v -- scroll page down
+		'M-86': scroll((x, y) => { return {x, y: y - window.innerHeight}; }), // M-v -- scroll page up
+		'M-S-188': scroll((x, y) => { return {x, y: 0}; }), // M-< -- scroll top
+		'M-S-190': scroll((x, y) => { return {x, y: window.scrollMaxY}; }), // M-> -- scroll bottom
+		'C-88, 75': sendActionMessage('closeTab'), // C-x, k -- close the current tab
+		'C-88, 70': sendActionMessage('openNewTab'), // C-x, f -- open a new tab
 };
+
+var prevKeyStroke = '';
 
 document.addEventListener('keydown', onKeyDown);
 
 function onKeyDown(event) {
 		var keyStroke = '';
-
 		if (event.altKey || event.metaKey) {
 				keyStroke += 'M-';
 		}
-
-
 		if (event.ctrlKey) {
 				keyStroke += 'C-';
 		}
-
 		if (event.shiftKey) {
 				keyStroke += 'S-';
 		}
-
 		keyStroke += event.keyCode;
 
-		console.log('Trying keystroke ' + keyStroke);
-		// if the keystroke is defined
-		if (!keyStrokes.hasOwnProperty(keyStroke)) {
-				console.log('Not found');
-				return;
-		}
+		const longKeyStroke = `${prevKeyStroke}, ${keyStroke}`;
+		var action = keyStrokes[longKeyStroke] || keyStrokes[keyStroke] || noop;
 
+		prevKeyStroke = keyStroke;
 
 		// if the keystroke is handled we prevent the default behavior
-		if (keyStrokes[keyStroke](event)) {
-				console.log('handled');
+		if (action(event)) {
+				prevKeyStroke = '';
 				event.preventDefault();
 		}
+}
+
+function sendActionMessage(name) {
+		return () => {
+				chrome.runtime.sendMessage({action: name});
+		};
 }
 
 function getScrollSteps() {
@@ -82,3 +82,5 @@ function scroll(mutator) {
 				return true;
 		}
 }
+
+function noop() { return false; }
